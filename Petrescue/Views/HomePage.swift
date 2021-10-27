a0a425d5081c619ac5b7d91476f668e885abaa10
Ethan <rxswift@126.com>
2021-10-27 14:14:39 +0800

    添加cocoapods

--- Petrescue/Views/HomePage.swift
+++ Petrescue/Views/HomePage.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct HomePage: View {
     var body: some View {
-        Text("Hello, HomePage!")
+        VStack {
+            HStack {
+                Text("begin")
+            }
+        }
     }
 }
 
